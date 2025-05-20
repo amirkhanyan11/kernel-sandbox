d7f9368cff11d8799673b95c25fe1f23c7e0977e
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-05-20 15:43:18 +0400

    .

--- hello_cdev.c
+++ hello_cdev.c
@@ -1,25 +1,63 @@
 #include "linux/fs.h"
+#include "linux/gfp_types.h"
+#include "linux/init.h"
+#include "linux/kern_levels.h"
 #include "linux/printk.h"
+#include "linux/slab.h"
 #include "linux/types.h"
+#include "linux/uaccess.h"
 #include <linux/kernel.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Mickael Jackson");
-MODULE_DESCRIPTION("Character driver");
-MODULE_VERSION("1.4.88");
 
 #define DEVICE_NAME "hello_cdev"
+#define BUFFER_SIZE 2048
 
 static int major = 0;
+uint8_t *kernel_buffer = NULL;
 
-ssize_t my_read(struct file *file, char __user *user, size_t s, loff_t *l) {
-  printk("Called my_read");
+
+static int my_release(struct inode *inode, struct file *file) {
+  kfree(kernel_buffer);
+  printk(KERN_INFO "Called release, successfully deallocated memory of the kernel buffer");
+  return 0;
+}
+
+static int my_open(struct inode *inode, struct file *file) {
+  kernel_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+  if (NULL == kernel_buffer) {
+    printk(KERN_ERR "bad alloc :(");
+    return -1;
+  }
+  printk(KERN_INFO "Called open, successfully allocated %d bytes for the kernel buffer", BUFFER_SIZE);
+  return 0;
+}
+
+static ssize_t my_write(struct file *file, const char __user *buffer, size_t n, loff_t *l) {
+  if (0 != copy_from_user(kernel_buffer, buffer, n)) {
+    printk(KERN_ERR "Error writing data to kernel buffer");
+    return -1;
+  }
+  printk(KERN_INFO "Called write, successfully wrote %zu bytes", n);
+  return 0;
+}
+
+static ssize_t my_read(struct file *file, char __user *buffer, size_t n, loff_t *l) {
+  if (0 != copy_to_user(buffer, kernel_buffer, n)) {
+    printk(KERN_ERR "Error fetching data from kernel buffer");
+    return -1;
+  }
+  printk("Called read, successfully read %zu bytes", n);
   return 0;
 }
 
 static struct file_operations fops = {
-  .read = my_read
+  .owner = THIS_MODULE,
+  .open = my_open,
+  .release = my_release,
+  .read = my_read,
+  .write = my_write
 };
 
 static int __init hello_mod_init(void) {
@@ -34,7 +72,7 @@ static int __init hello_mod_init(void) {
 
 static void __exit hello_mod_exit(void) {
   unregister_chrdev(major, DEVICE_NAME);
-  printk(KERN_INFO "Exiting hello world module from kernel !!!\n");
+  printk(KERN_INFO "Exiting chrdev module from kernel !!!\n");
 }
 
 module_init(hello_mod_init);
