97e194493c4077e8502baa50ae9cf5e5f0159fb1
Artyom Amirkhanyan <artyom.amirkhanyan@taurotech.com>
2025-05-21 13:33:20 +0400

    moved memory allocation from read to init

--- hello_cdev.c
+++ hello_cdev.c
@@ -16,52 +16,63 @@ MODULE_LICENSE("GPL");
 
 static int major = 0;
 uint8_t *kernel_buffer = NULL;
-
+static uint32_t data_size = 0;
 
 static int my_release(struct inode *inode, struct file *file) {
-  kfree(kernel_buffer);
-  printk(KERN_INFO "Called release, successfully deallocated memory of the kernel buffer");
+  printk(KERN_INFO "Called release");
   return 0;
 }
 
 static int my_open(struct inode *inode, struct file *file) {
+  data_size = 0;
+  file->f_pos = 0;
+  printk(KERN_INFO "Called open");
+  return 0;
+}
+
+static ssize_t my_read(struct file *file, char __user *buf, size_t count, loff_t *ppos) {
+  if (*ppos >= data_size)
+    return 0;
+  if (count > data_size - *ppos)
+    count = data_size - *ppos;
+
+  if (copy_to_user(buf, kernel_buffer + *ppos, count))
+    return -1;
+  *ppos += count;
+  return count;
+}
+
+static ssize_t my_write(struct file *file, const char __user *buf, size_t count,
+                        loff_t *ppos) {
+  if (*ppos >= BUFFER_SIZE)
+    return -ENOMEM;
+
+  if (count > BUFFER_SIZE - *ppos)
+    count = BUFFER_SIZE - *ppos;
+
+  if (copy_from_user(kernel_buffer + *ppos, buf, count))
+    return -1;
+  *ppos += count;
+  data_size = *ppos;
+  return count;
+}
+
+static struct file_operations fops = {.owner = THIS_MODULE,
+                                      .open = my_open,
+                                      .release = my_release,
+                                      .read = my_read,
+                                      .write = my_write};
+
+static int __init hello_mod_init(void) {
+  major = register_chrdev(0, DEVICE_NAME, &fops);
+
   kernel_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+  memset(kernel_buffer, 0, BUFFER_SIZE);
   if (NULL == kernel_buffer) {
     printk(KERN_ERR "bad alloc :(");
     return -1;
   }
-  printk(KERN_INFO "Called open, successfully allocated %d bytes for the kernel buffer", BUFFER_SIZE);
-  return 0;
-}
 
-static ssize_t my_write(struct file *file, const char __user *buffer, size_t n, loff_t *l) {
-  if (0 != copy_from_user(kernel_buffer, buffer, n)) {
-    printk(KERN_ERR "Error writing data to kernel buffer");
-    return -1;
-  }
-  printk(KERN_INFO "Called write, successfully wrote %zu bytes", n);
-  return 0;
-}
-
-static ssize_t my_read(struct file *file, char __user *buffer, size_t n, loff_t *l) {
-  if (0 != copy_to_user(buffer, kernel_buffer, n)) {
-    printk(KERN_ERR "Error fetching data from kernel buffer");
-    return -1;
-  }
-  printk("Called read, successfully read %zu bytes", n);
-  return 0;
-}
-
-static struct file_operations fops = {
-  .owner = THIS_MODULE,
-  .open = my_open,
-  .release = my_release,
-  .read = my_read,
-  .write = my_write
-};
-
-static int __init hello_mod_init(void) {
-  major = register_chrdev(0, DEVICE_NAME, &fops);
   if (major < 0) {
     printk("Error while registering char device");
     return major;
@@ -72,6 +83,7 @@ static int __init hello_mod_init(void) {
 
 static void __exit hello_mod_exit(void) {
   unregister_chrdev(major, DEVICE_NAME);
+  kfree(kernel_buffer);
   printk(KERN_INFO "Exiting chrdev module from kernel !!!\n");
 }
 
